b65448bf34825a8a283263b51a1ec9e3d3b06142
Diego Boggle <diegoboggle@icloud.com>
2025-10-08 22:01:18 -0300

    Se crea y configura el build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,59 +1,51 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("com.android.application") // Indica que es una app de Android.
+    id("org.jetbrains.kotlin.android") // Permite usar Kotlin.
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17"  // Autoescribe código.
+    id("kotlin-parcelize") // Permite pasar datos entre pantallas.
 }
 
 android {
-    namespace = "com.martinvergara_diegoboggle.pawschedule"
-    compileSdk = 36
+    namespace = "com.martinvergara_diegoboggle.pawschedule" // Nombre del proyecto.
+    compileSdk = 36 // Versión de Android.
 
     defaultConfig {
-        applicationId = "com.martinvergara_diegoboggle.pawschedule"
-        minSdk = 36
-        targetSdk = 36
+        applicationId = "com.martinvergara_diegoboggle.pawschedule" // Identificador de la app.
+        minSdk = 24 // La versión más antigua de Android que puede usarla.
+        targetSdk = 36// La versión para la que está optimizada.
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
+
     buildFeatures {
-        compose = true
+        viewBinding = true
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
+    implementation(libs.androidx.core.ktx) // Utilidades básicas de Kotlin para Android.
+    implementation(libs.androidx.appcompat) // Para que la app sea compatible con versiones antiguas de Android.
+    implementation(libs.material) // Componentes visuales: botones, menús, etc.
+    implementation(libs.androidx.constraintlayout) // Para organizar los elementos en la pantalla.
+    implementation(libs.androidx.navigation.fragment.ktx) // Gestiona la navegación.
+    implementation(libs.androidx.navigation.ui.ktx) // Conecta la navegación con la interfaz.
+    implementation(libs.androidx.lifecycle.viewmodel.ktx) // El cerebro de cada pantalla, donde va la lógica.
+    implementation(libs.androidx.lifecycle.livedata.ktx) // Permite que la pantalla reaccione a cambios en los datos.
+    implementation(libs.androidx.room.runtime) // La base de datos.
+    implementation(libs.androidx.room.ktx) // Ayudas para usar la base de datos con Kotlin.
+    ksp(libs.androidx.room.compiler.v261) // Genera el código necesario para que Room funcione.
+    implementation(libs.kotlinx.coroutines.android) // Evita que la app se congele.
+    testImplementation(libs.junit) // Framework de pruebas.
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
